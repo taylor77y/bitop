aff247633f168b0c10f8931e878b4aff629023c1
Administrator <taylor77y@gmail.com>
2021-12-29 15:04:59 -0800

    Initial commit

---
+++ src/main/java/com/fcg/bitop/BitopApplication.java
@@ -0,0 +1,13 @@
+package com.fcg.bitop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitopApplication.class, args);
+    }
+
+}
